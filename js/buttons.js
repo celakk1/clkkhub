958676cf76cfbea0a28ba9dac2fa4d66d73478d0
celakk1 <norbi360@icloud.com>
2025-09-12 19:40:02 +0200

    added changelog and settings popup

--- js/buttons.js
+++ js/buttons.js
@@ -3,3 +3,33 @@ document.querySelectorAll(".button").forEach(btn => {
          window.open(btn.dataset.link, '_blank')
      })
 });
+
+function togglesettings() {
+    const settings = document.getElementById('settingspanel');
+
+    settings.classList.toggle('active');
+    settings.classList.toggle('item-hidden')
+
+    updatemenupanel();
+}
+
+function togglechangelog() {
+    const changelog = document.getElementById('changelogpanel');
+
+    changelog.classList.toggle('active');
+    changelog.classList.toggle('item-hidden')
+
+    updatemenupanel()
+}
+
+function updatemenupanel() {
+    const settings = document.getElementById('settingspanel');
+    const changelog = document.getElementById('changelogpanel');
+    const menupanel = document.getElementById('menupanel');
+
+    if (!settings.classList.contains('active') && !changelog.classList.contains('active')) {
+        menupanel.classList.remove('active');
+    } else {
+        menupanel.classList.add('active');
+    }
+}
